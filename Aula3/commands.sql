81c11dfd553ad1677f61915421ecd385bb38b55e
Yan <yannunes.araujo@gmail.com>
2025-08-26 23:54:29 -0300

    Criando tabela Professores

--- Aula3/commands.sql
+++ Aula3/commands.sql
@@ -1,9 +1,11 @@
 -- database: ./db.sqlite
 CREATE TABLE "Professores" (
-    "ID_Professor" INTEGER NOT NULL,
+    "ID_Professor" INTEGER NOT NULL PRIMARY KEY,
     "Nome" TEXT NOT NULL,
     "ID_Disciplina" INTEGER NOT NULL,
     "Matricula" TEXT NOT NULL,
 
     FOREIGN KEY ("ID_Disciplina") REFERENCES "Disciplina"
 );
+
+DROP TABLE "Professores";
